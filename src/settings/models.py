9f31e6391ad22f67944059af98c3303706268b58
LedxDeliveryFlopp <korstim18@gmail.com>
2024-05-05 18:48:16 +0700

    add Base model

---
+++ src/settings/models.py
@@ -0,0 +1,11 @@
+from sqlalchemy import Column, Integer, DateTime, func
+from src.settings.database import Base
+
+
+class AbstractModel(Base):
+    """Абстрактная модель"""
+    __abstract__ = True
+
+    id = Column(Integer,  primary_key=True, nullable=False, autoincrement=True, unique=True,
+                index=True, comment="ID")
+    created_at = Column(DateTime, server_default=func.now())
