9e48457f1ed57075a82884f8bab9c2f136cd9411
LedxDeliveryFlopp <korstim18@gmail.com>
2024-05-07 16:53:57 +0700

    add except database exceptions in BaseRepository

--- src/settings/repository.py
+++ src/settings/repository.py
@@ -1,5 +1,5 @@
 from dataclasses import dataclass
-from sqlalchemy.exc import IntegrityError
+from sqlalchemy.exc import IntegrityError, ProgrammingError
 from sqlalchemy.ext.asyncio import AsyncSession
 
 
@@ -8,7 +8,7 @@ class BaseRepository:
     """Базовый репозиторий для работы с БД"""
     session: AsyncSession
 
-    async def session_save_object(self, saved_object: object) -> None:
+    async def session_save_object(self, saved_object: object) -> dict:
         """Сохрание объекта в БД"""
         try:
             self.session.add(saved_object)
@@ -16,11 +16,17 @@ class BaseRepository:
             await self.session.refresh(saved_object)
         except IntegrityError:
             await self.session.rollback()
+            return {"detail": "database exception"}
+        except ProgrammingError:
+            return {"detail": "database exception"}
 
-    async def session_delete_object(self, deleted_object: object) -> None:
+    async def session_delete_object(self, deleted_object: object) -> dict:
         """Удаление из БД"""
         try:
             await self.session.delete(deleted_object)
             await self.session.commit()
         except IntegrityError:
             await self.session.rollback()
+            return {"detail": "database exception"}
+        except ProgrammingError:
+            return {"detail": "database exception"}
